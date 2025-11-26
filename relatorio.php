4e1afe9e2510dee6f83e9a477ff12fbaf814edb5
troler11 <lucasbubu22@gmail.com>
2025-11-25 22:50:15 -0300

    Update relatorio.php

--- relatorio.php
+++ relatorio.php
@@ -100,7 +100,7 @@ foreach ($linhas_totais as $l) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ABM Bus - Relatório Operacional</title>
+    <title> Viação Mimo - Relatório Operacional</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
     <style>
@@ -374,3 +374,4 @@ const cellRest = document.getElementById('tempo-rest-' + placa);
 
 </body>
 </html>
+
