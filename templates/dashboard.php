43621f860a3404cc2f6b98625b5857ed0140c3f9
troler11 <lucasbubu22@gmail.com>
2025-11-25 22:51:05 -0300

    Update print statement from 'Hello' to 'Goodbye'

--- templates/dashboard.php
+++ templates/dashboard.php
@@ -51,7 +51,7 @@ $primeiro_veiculo_json = json_encode($todas_linhas[0] ?? null);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ABM Bus - Dashboard</title>
+    <title>Viação Mimo - Dashboard</title>
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
