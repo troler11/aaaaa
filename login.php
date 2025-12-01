7a289ba30bd8bbe5aea6b257f9f261e5c4b013ff
troler11 <lucasbubu22@gmail.com>
2025-12-01 11:11:39 -0300

    Update login.php

--- login.php
+++ login.php
@@ -136,7 +136,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         </form>
     </div>
     <div class="card-footer text-center py-3 bg-light">
-        <small class="text-muted">&copy; <?php echo date('Y'); ?> ABM Bus System</small>
+        <small class="text-muted">&copy; <?php echo date('Y'); ?> Viação Mimo</small>
+         <small class="text-muted">Desenvolvido por: Lucas Bueno</small>
     </div>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
